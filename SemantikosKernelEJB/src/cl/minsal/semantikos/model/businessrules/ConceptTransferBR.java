430a1d76d91c849831ed55fb4e56ebac10d61895
Andres Farias <andres.farias@blueprintsit.cl>
2016-11-24 12:07:19 -0300

    Se agrega un composite

--- SemantikosKernelEJB/src/cl/minsal/semantikos/model/businessrules/ConceptTransferBR.java
+++ SemantikosKernelEJB/src/cl/minsal/semantikos/model/businessrules/ConceptTransferBR.java
@@ -31,6 +31,9 @@ public class ConceptTransferBR {
 
         /* Sólo se pueden trasladar Conceptos Modelados y que no tengan Relaciones a otros Conceptos Semantikos */
         preCondition001(conceptSMTK);
+
+        /* No se pueden trasladar el concepto "Pendientes" ni el concepto No Validos de la categoría Concepto Especial */
+        preCondition002(conceptSMTK);
     }
 
     /**
@@ -41,8 +44,27 @@ public class ConceptTransferBR {
      */
     private void preCondition001(ConceptSMTK conceptSMTK) {
 
-        if (!conceptSMTK.isModeled() || !conceptSMTK.getRelationshipsTo(TargetType.SMTK).isEmpty()){
+        if (!conceptSMTK.isModeled() || !conceptSMTK.getRelationshipsTo(TargetType.SMTK).isEmpty()) {
             throw new BusinessRuleException("BR-TRANS-001" + "Sólo se pueden trasladar Conceptos Modelados y que no tengan Relaciones a otros Conceptos Semantikos");
         }
+    }
+
+    /**
+     * BR-TRANS-002: El Concepto “Pendientes” y el Concepto “No Válidos” Modelados de categoría Concepto Especial no se
+     * pueden Trasladar.
+     *
+     * @param conceptSMTK El concepto que se quiere trasladar.
+     */
+    private void preCondition002(ConceptSMTK conceptSMTK) {
+
+        ConceptSMTK noValidConcept = conceptManager.getNoValidConcept();
+        ConceptSMTK pendingConcept = null; //TODO: conceptManager.getNoValidConcept();
+
+        /* Se valida que el concepto en cuestión no sea ni el no valido ni el pendientes */
+        if (conceptSMTK.equals(noValidConcept) || conceptSMTK.equals(pendingConcept)){
+            throw new BusinessRuleException("BR-TRANS-002" + "El Concepto “Pendientes” y el Concepto “No Válidos” " +
+                    "Modelados de categoría Concepto Especial no se pueden Trasladar.");
+        }
+
     }
 }
