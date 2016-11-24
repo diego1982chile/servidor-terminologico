b781751e5bb51002d417fd3e59dec3b07dd94d73
Andres Farias <andres.farias@blueprintsit.cl>
2016-11-24 14:16:02 -0300

    Se desarrolla el composite para mostrar la categoría del traslado.

---
+++ SemantikosWeb/src/cl/minsal/semantikos/designer_modeler/designer/TransferConceptBean.java
@@ -0,0 +1,22 @@
+package cl.minsal.semantikos.designer_modeler.designer;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ * @author Andrés Farías on 11/24/16.
+ */
+
+@ManagedBean(name = "transferConceptBean")
+public class TransferConceptBean {
+
+    /** El ID de la categoría destino */
+    private long categoryId;
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(long categoryId) {
+        this.categoryId = categoryId;
+    }
+}
