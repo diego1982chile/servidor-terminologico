8c7c3f6b167c69805bf03e42f07e0b736a52542a
Gustavo Punucura <gustavopunucura@gmail.com>
2016-11-30 17:43:42 -0300

    Fix RefsetAudit

--- SemantikosKernelEJB/src/cl/minsal/semantikos/model/audit/RefSetAuditAction.java
+++ SemantikosKernelEJB/src/cl/minsal/semantikos/model/audit/RefSetAuditAction.java
@@ -1,5 +1,6 @@
 package cl.minsal.semantikos.model.audit;
 
+import cl.minsal.semantikos.model.ConceptSMTK;
 import cl.minsal.semantikos.model.RefSet;
 import cl.minsal.semantikos.model.User;
 
@@ -12,6 +13,10 @@ public class RefSetAuditAction extends AuditAction{
 
     private final RefSet refSet;
 
+    public RefSetAuditAction(RefSet refSet, AuditActionType refsetCreation, Timestamp now, User user, ConceptSMTK concept) {
+        super(refsetCreation, now, user, refSet,concept);
+        this.refSet = refSet;
+    }
     public RefSetAuditAction(RefSet refSet, AuditActionType refsetCreation, Timestamp now, User user) {
         super(refsetCreation, now, user, refSet);
         this.refSet = refSet;
