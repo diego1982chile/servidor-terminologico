2daa50351d7d951dcc50cef41db92dc7a6d36dc8
Gustavo Punucura <gustavopunucura@gmail.com>
2016-12-07 18:03:30 -0300

    Incidencia     ST-445 Hito3-V2.1.1 -012 al grabar, ERROR

--- SemantikosKernelEJB/src/cl/minsal/semantikos/kernel/components/DescriptionManagerImpl.java
+++ SemantikosKernelEJB/src/cl/minsal/semantikos/kernel/components/DescriptionManagerImpl.java
@@ -168,14 +168,17 @@ public class DescriptionManagerImpl implements DescriptionManager {
         }
     }
 
+    @EJB
+    private DescriptionTranslationBR descriptionTranslationBR;
+
     @Override
     public void moveDescriptionToConcept(ConceptSMTK sourceConcept, Description description, User user) {
 
         ConceptSMTK targetConcept = description.getConceptSMTK();
 
         /* Se aplican las reglas de negocio para el traslado */
-        DescriptionTranslationBR descriptionTranslationBR = new DescriptionTranslationBR();
-        descriptionTranslationBR.validatePreConditions(description, targetConcept);
+
+        descriptionTranslationBR.validatePreConditions(sourceConcept, description, targetConcept);
 
         /* Se realiza la actualización a nivel del modelo lógico */
 
@@ -190,7 +193,7 @@ public class DescriptionManagerImpl implements DescriptionManager {
         description.setConceptSMTK(targetConcept);
 
         /* Se aplican las reglas de negocio asociadas al movimiento de un concepto */
-        descriptionTranslationBR.apply(targetConcept, description);
+        descriptionTranslationBR.apply(sourceConcept,targetConcept, description);
 
         /*Se cambia el estado de la descripción segun el concepto*/
 
