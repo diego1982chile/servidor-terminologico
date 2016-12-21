6dad6caf4595335a8db2031fbb35e4a2fcb37297
diego1982chile <diego.abelardo.soto@gmail.com>
2016-12-21 16:03:58 -0300

    se trabaj en incidencias varias

--- SemantikosKernelEJB/src/cl/minsal/semantikos/kernel/components/QueryManager.java
+++ SemantikosKernelEJB/src/cl/minsal/semantikos/kernel/components/QueryManager.java
@@ -17,32 +17,110 @@ import java.util.List;
 @Local
 public interface QueryManager {
 
+    /**
+     * Este método es responsable de inicializar un objeto de consulta para el navegador de categorías,
+     * Dada una categoría en particular.
+     *
+     * @param category La categoría a consultar
+     */
     public GeneralQuery getDefaultGeneralQuery(Category category);
 
+    /**
+     * Este método es responsable de inicializar un objeto de consulta para el navegador de descripciones,
+     *
+     */
     public DescriptionQuery getDefaultDescriptionQuery();
 
+    /**
+     * Este método es responsable de inicializar un objeto de consulta para el navegador de no válidos,
+     *
+     */
     public NoValidQuery getDefaultNoValidQuery();
 
+    /**
+     * Este método es responsable de inicializar un objeto de consulta para el navegador de pendientes,
+     *
+     */
     public PendingQuery getDefaultPendingQuery();
 
+    /**
+     * Este método es responsable de ejecutar una consulta en el navegador de categorías,
+     * Dado el objeto de consulta correspondiente
+     *
+     * @param query El objeto de consulta para el navegador de categorías
+     */
     public List<ConceptSMTK> executeQuery(GeneralQuery query);
 
+    /**
+     * Este método es responsable de ejecutar una consulta en el navegador de descripciones,
+     * Dado el objeto de consulta correspondiente
+     *
+     * @param query El objeto de consulta para el navegador de descripciones
+     */
     public List<Description> executeQuery(DescriptionQuery query);
 
+    /**
+     * Este método es responsable de ejecutar una consulta en el navegador de no válidos,
+     * Dado el objeto de consulta correspondiente
+     *
+     * @param query El objeto de consulta para el navegador de no válidos
+     */
     public List<NoValidDescription> executeQuery(NoValidQuery query);
 
+    /**
+     * Este método es responsable de ejecutar una consulta en el navegador de pendientes,
+     * Dado el objeto de consulta correspondiente
+     *
+     * @param query El objeto de consulta para el navegador de pendientes
+     */
     public List<PendingTerm> executeQuery(PendingQuery query);
 
+    /**
+     * Este método es responsable de contabilizar los resultados de una consulta en el navegador de categorías,
+     * Dado el objeto de consulta correspondiente
+     *
+     * @param query El objeto de consulta para el navegador de categorías
+     */
     public int countQueryResults(GeneralQuery query);
 
+    /**
+     * Este método es responsable de contabilizar los resultados de una consulta en el navegador de descripciones,
+     * Dado el objeto de consulta correspondiente
+     *
+     * @param query El objeto de consulta para el navegador de descripciones
+     */
     public int countQueryResults(DescriptionQuery query);
 
+    /**
+     * Este método es responsable de contabilizar los resultados de una consulta en el navegador de no válidos,
+     * Dado el objeto de consulta correspondiente
+     *
+     * @param query El objeto de consulta para el navegador de no válidos
+     */
     public int countQueryResults(NoValidQuery query);
 
+    /**
+     * Este método es responsable de contabilizar los resultados de una consulta en el navegador de pendientes,
+     * Dado el objeto de consulta correspondiente
+     *
+     * @param query El objeto de consulta para el navegador de pendientes
+     */
     public int countQueryResults(PendingQuery query);
 
+    /**
+     * Este método es responsable de obtener los atributos filtrables de una categoría para el navegador de categorias,
+     * Dado una categoría en particular
+     *
+     * @param category El objeto de consulta para el navegador de pendientes
+     */
     public List<RelationshipDefinition> getSearchableAttributesByCategory(Category category);
 
+    /**
+     * Este método es responsable de obtener los atributos desplegables de una categoría para el navegador de categorias,
+     * Dado una categoría en particular
+     *
+     * @param category El objeto de consulta para el navegador de pendientes
+     */
     public List<RelationshipDefinition> getShowableAttributesByCategory(Category category);
 
 }
