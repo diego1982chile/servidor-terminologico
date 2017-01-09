f4c24d582e5605cda5495129afe8debf83df06a0
Andres Farias R <andres.farias@blueprintsit.cl>
2017-01-09 11:24:28 -0300

    Se corrige una clase autogenerada.

--- SemantikosClientesWS/ClienteJava/src/cl/minsal/semantikos/ws/shared/CodificacionDeNuevoTermino.java
+++ SemantikosClientesWS/ClienteJava/src/cl/minsal/semantikos/ws/shared/CodificacionDeNuevoTermino.java
@@ -32,7 +32,7 @@ import javax.xml.bind.annotation.XmlType;
 })
 public class CodificacionDeNuevoTermino {
 
-    @XmlElement(namespace = "http://service.ws.semantikos.minsal.cl/", required = true)
+    @XmlElement(namespace = "", required = true)
     protected PeticionCodificacionDeNuevoTermino peticionCodificacionDeNuevoTermino;
 
     /**
